fb284d89ef5bcdf5ab396f3b9efc9cf9d0d1354d
yiming <100650920@qq.com>
2024-06-28 09:30:20 +0000

    Fix smj ordering

--- starry-core/src/main/scala/org/apache/spark/sql/execution/dict/ExecutorDictManager.scala
+++ starry-core/src/main/scala/org/apache/spark/sql/execution/dict/ExecutorDictManager.scala
@@ -21,6 +21,7 @@ import org.apache.spark.broadcast.TorrentBroadcast
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.execution.columnar.jni.{NativeColumnVector, NativeExpressionConvert}
 import org.apache.spark.sql.execution.columnar.{VeloxColumnarBatch, VeloxWritableColumnVector}
+import org.apache.spark.sql.execution.dict.ExecutorDictManager.dictCache
 import org.apache.spark.sql.types.{DataType, StringType, StructField, StructType}
 import org.apache.spark.sql.vectorized.ColumnVector
 
@@ -28,49 +29,31 @@ import java.lang
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicLong
 
-trait IDict
+trait IDict extends Logging {
+  def eval(totalBytes: AtomicLong): VeloxColumnarBatch
+}
 
-case class Dict(bytes: Array[Byte]) extends IDict
+case class Dict(bytes: Array[Byte]) extends IDict {
+  override def eval(totalBytes: AtomicLong): VeloxColumnarBatch = {
+    val start = System.currentTimeMillis()
+    val schema = StructType(Seq(StructField("dict", StringType)))
+    val batch = VeloxColumnarBatch.createFromJson(bytes, schema)
+    batch.setSchema(schema)
+    log.info(
+      s"create execution dict vector take time ${System.currentTimeMillis() - start}," +
+        s" total bytes${totalBytes.addAndGet(bytes.length)}")
+    batch
+  }
+}
 
-case class ExecutionDict(dict: Long, numBlocks: Int, dataType: DataType,
-                         nativeExpression: String) extends IDict
-
-object ExecutorDictManager extends Logging {
-  private val totalBytes = new AtomicLong(0)
-
-  // todo make size from config
-  val dictCache: LoadingCache[(java.lang.Long, java.lang.Integer), VeloxColumnarBatch] = CacheBuilder.newBuilder
-    .maximumSize(200)
-    .removalListener(
-      (notify: RemovalNotification[(java.lang.Long, java.lang.Integer), VeloxColumnarBatch]) => {
-        log.info("remove dict vector cache ")
-        notify.getValue.close()
-      })
-    .expireAfterAccess(1, TimeUnit.HOURS)
-    .build(new CacheLoader[(java.lang.Long, java.lang.Integer), VeloxColumnarBatch]() {
-      override def load(key: (lang.Long, Integer)): VeloxColumnarBatch = loadDict(key._1, key._2)
-    })
-
-  val executionDictCache: LoadingCache[ExecutionDict, VeloxColumnarBatch] = CacheBuilder.newBuilder
-    .maximumSize(50)
-    .removalListener(
-      (notify: RemovalNotification[ExecutionDict, VeloxColumnarBatch]) => {
-        log.info("remove execution dict vector cache ")
-        notify.getValue.close()
-      })
-    .expireAfterAccess(1, TimeUnit.HOURS)
-    .build(new CacheLoader[ExecutionDict, VeloxColumnarBatch]() {
-      def load(dict: ExecutionDict): VeloxColumnarBatch = {
-        eval(dict)
-      }
-    })
-
-  private def eval(dict: ExecutionDict): VeloxColumnarBatch = {
+case class ExecutionDict(dict: Long, numBlocks: Int, dataType: DataType, nativeExpression: String)
+    extends IDict {
+  override def eval(totalBytes: AtomicLong): VeloxColumnarBatch = {
     try {
       val start = System.currentTimeMillis()
-      val batch = dictCache.get((dict.dict, dict.numBlocks))
-      val schema = StructType(Seq(StructField("dict", dict.dataType)))
-      val result = NativeExpressionConvert.evalWithBatch(dict.nativeExpression, batch, schema)
+      val batch = dictCache.get((dict, numBlocks))
+      val schema = StructType(Seq(StructField("dict", dataType)))
+      val result = NativeExpressionConvert.evalWithBatch(nativeExpression, batch, schema)
       log.info(
         s"create dict vector take time ${System.currentTimeMillis() - start}," +
           s" total bytes${totalBytes.addAndGet(-1)}")
@@ -81,32 +64,51 @@ object ExecutorDictManager extends Logging {
         null
     }
   }
+}
+
+object ExecutorDictManager extends Logging {
+  private val totalBytes = new AtomicLong(0)
+
+  // todo make size from config
+  val dictCache: LoadingCache[(java.lang.Long, java.lang.Integer), VeloxColumnarBatch] =
+    CacheBuilder.newBuilder
+      .maximumSize(200)
+      .removalListener((notify: RemovalNotification[
+        (java.lang.Long, java.lang.Integer),
+        VeloxColumnarBatch]) => {
+        log.info("remove dict vector cache ")
+        notify.getValue.close()
+      })
+      .expireAfterAccess(1, TimeUnit.HOURS)
+      .build(new CacheLoader[(java.lang.Long, java.lang.Integer), VeloxColumnarBatch]() {
+        override def load(key: (lang.Long, Integer)): VeloxColumnarBatch =
+          loadDict(key._1, key._2)
+      })
+
+  val executionDictCache: LoadingCache[ExecutionDict, VeloxColumnarBatch] =
+    CacheBuilder.newBuilder
+      .maximumSize(50)
+      .removalListener((notify: RemovalNotification[ExecutionDict, VeloxColumnarBatch]) => {
+        log.info("remove execution dict vector cache ")
+        notify.getValue.close()
+      })
+      .expireAfterAccess(1, TimeUnit.HOURS)
+      .build(new CacheLoader[ExecutionDict, VeloxColumnarBatch]() {
+        def load(dict: ExecutionDict): VeloxColumnarBatch = {
+          dict.eval(totalBytes)
+        }
+      })
+
 
   private def loadDict(bid: Long, numBlocks: Int): VeloxColumnarBatch = {
     val value = new TorrentBroadcast[IDict](null, bid, Some(numBlocks))
     try {
-      value.value match {
-        case dict: Dict =>
-          createDictBatch(dict)
-        case executionDict: ExecutionDict =>
-          eval(executionDict)
-      }
+      value.value.eval(totalBytes)
     } catch {
       case e: Throwable =>
         e.printStackTrace()
         null
     }
-  }
-
-  private def createDictBatch(dict: Dict): VeloxColumnarBatch = {
-    val start = System.currentTimeMillis()
-    val schema = StructType(Seq(StructField("dict", StringType)))
-    val batch = VeloxColumnarBatch.createFromJson(dict.bytes, schema)
-    batch.setSchema(schema)
-    log.info(
-      s"create execution dict vector take time ${System.currentTimeMillis() - start}," +
-        s" total bytes${totalBytes.addAndGet(dict.bytes.length)}")
-    batch
   }
 
   def fetchDictVectorAddress(bid: Long, numBlocks: Int): NativeColumnVector = {
